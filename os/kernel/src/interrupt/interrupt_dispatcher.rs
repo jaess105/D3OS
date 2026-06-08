8e0239d6cbace657166cfa473dda1fb42aa580ce
Richard Schweitzer <risch114@uni-duesseldorf.de>
2026-06-08 16:46:25 +0200

    panic and logging should now be heap-less; re-added panic to handle_page_fault

--- os/kernel/src/interrupt/interrupt_dispatcher.rs
+++ os/kernel/src/interrupt/interrupt_dispatcher.rs
@@ -212,9 +212,7 @@ fn handle_page_fault(frame: InterruptStackFrame, _index: u8, error: Option<u64>)
     let fault_addr = Cr2::read().expect("Invalid address in CR2 during page fault");
     let thread = scheduler().try_get_current_thread();
     if thread.is_none() {
-        info!("******** Page fault not handled because scheduler is locked **********");
-        return ;
-//        panic!("Page Fault, cannot get lock to scheduler\nError code: [{:?}]\nAddress: [0x{:0>16x}]", error, fault_addr);
+        panic!("Page Fault, cannot get lock to scheduler\nError code: [{:?}]\nAddress: [0x{:0>16x}]", error, fault_addr);
     }
 
     let thread = thread.unwrap();
